518eb959cd8aafb9dfd61ea87b747e9e337b55aa
Alun King <alun.king1@southwales.ac.uk>
2025-02-24 10:14:12 +0000

    Initial Commit

---
+++ IntervalStopwatch Watch App/IntervalStopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntervalStopwatchApp.swift
+//  IntervalStopwatch Watch App
+//
+//  Created by Alun King on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct IntervalStopwatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IntervalStopwatch/IntervalStopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntervalStopwatchApp.swift
+//  IntervalStopwatch
+//
+//  Created by Alun King on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct IntervalStopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
