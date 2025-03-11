dbe89206dfb96244308540f9d8e2b4884ce0f167
Alun King <alun.king1@southwales.ac.uk>
2025-03-11 13:42:15 +0000

    Fixed some editing bugs, made the cloning static to improve the execution, and added a sorting method to the list.

---
+++ IntervalStopwatch/UtilityView/SortSelector.swift
@@ -0,0 +1,18 @@
+//
+//  SortSelector.swift
+//  IntervalStopwatch
+//
+//  Created by Alun King on 10/03/2025.
+//
+
+import SwiftUI
+
+struct SortSelector: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SortSelector()
+}
