8cf8d2c5a10c36f8ef6650ad154c0d2650aa112b
Alun King <alun.king1@southwales.ac.uk>
2025-09-03 19:19:15 +0100

    Added copyright license and copyright message in source code files.

--- Models/Compression.swift
+++ Models/Compression.swift
@@ -4,6 +4,20 @@
 //
 //  Created by Alun King on 30/08/2025.
 //
+/*
+ * Copyright 2025 Alun King
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 ///This file is used to turn the Workout into a JSON file, encode it using Base64 and compress it using gzip.
 ///By doing this, the workout can be displayed as a JSON file for others to share and load.
 import Foundation
